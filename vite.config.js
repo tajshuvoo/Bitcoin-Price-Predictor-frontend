1c80c9bebd11cbf2d4b08b769853035fa4d823a1
tajshuvoo <tajshuvo75@gmail.com>
2025-06-28 05:20:01 +0600

    .

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
-    allowedHosts: 'all'
+    host: true,
+    port: process.env.PORT || 4173,
+    allowedHosts: ['all'],
   }
 })
